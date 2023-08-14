8a063c3e725681b1969d84985f3973d768249308
igor <igor.ramos.c@hotmail.com>
2023-08-14 10:57:15 -0500

    add cors for Jhimi

--- src/main/java/com/biblioteca/biblioteca/controlador/SesionController.java
+++ src/main/java/com/biblioteca/biblioteca/controlador/SesionController.java
@@ -6,10 +6,7 @@ import com.biblioteca.biblioteca.utilidades.ProblemaDetalle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +22,7 @@ public class SesionController {
         return  sesionServicio.getALlUsuario();
     }
 
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
     @GetMapping("/login/{correo}")
     public ResponseEntity<?> loginUsuario(@PathVariable String correo){
         Usuario usuario = sesionServicio.loginUsuario(correo);
